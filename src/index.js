2ae67e60ee5618f9f6352065f37bdb8c44bb9e6d
Scead64 <tinor.jain@gmail.com>
2023-09-08 15:57:36 -0400

    Removed unused code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
 ReactDOM.render(element, document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     element
-//   </React.StrictMode>
-// );
 
 
 
